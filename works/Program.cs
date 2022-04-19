268ee3e8cf58aa2ceb07733295d23c7662fccea5
suzenhalil <suzenhalil@hotmail.com>
2022-04-19 16:06:23 +0300

    Proje dosyası ekle.

---
+++ works/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace works
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Soru
+
+            //miktar al
+            //20 den küçükse toplamdan %5
+            //20-50 %10
+            //50-100 %15
+            //100 den fazla %25
+            //birim fiyat 5 tl
+            //ödenecek tutar
+            #endregion
+
+            #region Soru 1
+
+            //2 sayı al
+            //işlem al ve yap
+
+
+            #endregion
+
+            #region Soru 2
+
+            //mevsime göre ayları bastır
+
+            #endregion
+
+            #region Soru 3
+
+            //girilen bi tam sayının kaç basamaklı olduğunu yazdır.
+
+
+            #endregion
+        }
+    }
+}
