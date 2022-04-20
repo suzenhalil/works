f5e90c3d0541fe4a925260abe84f15e0fc47390e
suzenhalil <suzenhalil@hotmail.com>
2022-04-20 13:39:07 +0300

    gg

--- Dongu/Program.cs
+++ Dongu/Program.cs
@@ -48,16 +48,54 @@ namespace Dongu
 
             #region Do While(Faktoriyel)
 
-            int a = 1, b = 5;
-            Console.WriteLine("Bir sayı giriniz.");
-            b = Convert.ToInt32(Console.ReadLine());
-            do
-            {
-                a = a * b;
-                b--;
-            } while (b > 0);
-            Console.WriteLine(a);
+            //int a = 1, b = 5;
+            //Console.WriteLine("Bir sayı giriniz.");
+            //b = Convert.ToInt32(Console.ReadLine());
+            //do
+            //{
+            //    a = a * b;
+            //    b--;
+            //} while (b > 0);
+            //Console.WriteLine(a);
 
+
+            #endregion
+
+            #region Fordongusu
+            //Console.WriteLine("Bir sayı giriniz");
+
+            //for (int i = Convert.ToInt32(Console.ReadLine()); i <= 1000; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            #endregion
+            #region Örnek 1
+
+            //1 ile 10 arasındaki çift sayıları yazdır mod kullanmadan
+
+
+            #endregion
+            #region Örnek 2
+
+            //1 ile 10 arasındak tek sayıları tersten yazdır
+            //for (int i = 9; i > 0; i--)
+            //    {
+            //    Console.WriteLine(i);
+
+            //    }
+
+            #endregion
+            #region Örnek 3
+
+            //ayın ilk gün pazartesi
+            //pazarları basın 1 aylık
+
+            //for (int i = 7; i < 30; i = i + 7)
+            //{
+            //    Console.WriteLine(i);
+            //}
 
             #endregion
 
